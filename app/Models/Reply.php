0917dadea27b9acdccd4d67ddfeb354ffac7fdcf
洪聖峻 <su3tldk394@gmail.com>
2023-09-07 16:27:51 +0800

    go

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Reply extends Model
 {
     use HasFactory;
+    protected $table = 'replies';
     protected $fillable = ['name', 'content', 'message_id'];
 
     public function message()
